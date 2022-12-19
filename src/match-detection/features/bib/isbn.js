d9bb6cfe58b30fc2a99e3702fc3d761c731bd426
Minttu Hurme <ammsalme@users.noreply.github.com>
2022-12-19 12:48:41 +0200

    Test (#30)
    * Add combined author query for short titles (#29)
    
    * Add combined author query for short titles
    * 3.0.3-alpha.1
    
    * Better isbn matching (#28)
    
    1) Updates to standardIdentifier/ISBN-matching
    
    Extract:
    
    * Extract standardIdentifiers from all fields instead of just the first
    * Extract every subfield (valid/invalid) and identifier combination just once
    * Get subfieldsCode(s) for valid subfield(s) as a parameter
    
    Compare:
    * Penalties for mismatched/missing identifiers in case of existing matching valid identifiers
    * If both records have valid identifiers but none of these match, give minus points
    * Get subfieldsCode(s) for valid and invalid subfield(s) as a parameter
    * Choose lesser amount of matches in case of matching A to B and matching B to A resulting in differing amount of matches
    
    General:
    * Add extractSubfieldsFromFields to matchingUtils
    * Add uniqueSubfields to matchingUtils
    
    * Fix tests broken by changes
    * Debug
    * Comments
    
    2) Updates to normalizing and validating ISBNs
    
    * Handle formally invalid ISBNs from f020 subfield $a as if they were extracted from f020 subfield $z
    * Use isbn3 module to normalize ISBN as non-hyphenated ISBN-13s if possible
    * Normalize hyphens out also from strings that are not valid ISBNs
    
    * 3.0.2-alpha.1
    
    * Bump @natlibfi/marc-record-serializers from 9.0.2 to 9.0.4 (#27)
    
    Bumps [@natlibfi/marc-record-serializers](https://github.com/natlibfi/marc-record-serializers) from 9.0.2 to 9.0.4.
    - [Release notes](https://github.com/natlibfi/marc-record-serializers/releases)
    - [Commits](https://github.com/natlibfi/marc-record-serializers/compare/v9.0.2...v9.0.4)
    
    ---
    updated-dependencies:
    - dependency-name: "@natlibfi/marc-record-serializers"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump @natlibfi/marc-record-serializers from 9.0.2 to 9.0.4 (#31)
    
    Bumps [@natlibfi/marc-record-serializers](https://github.com/natlibfi/marc-record-serializers) from 9.0.2 to 9.0.4.
    - [Release notes](https://github.com/natlibfi/marc-record-serializers/releases)
    - [Commits](https://github.com/natlibfi/marc-record-serializers/compare/v9.0.2...v9.0.4)
    
    ---
    updated-dependencies:
    - dependency-name: "@natlibfi/marc-record-serializers"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/match-detection/features/bib/isbn.js
+++ src/match-detection/features/bib/isbn.js
@@ -27,8 +27,27 @@
 */
 
 import createInterface from './standard-identifier-factory';
+import {parse as isbnParse} from 'isbn3';
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger(`@natlibfi/melinda-record-matching:match-detection:features:standard-identifiers:ISBN`);
+const debugData = debug.extend('data');
 
 export default () => {
-  const {extract, compare} = createInterface({pattern: /^020$/u, subfieldCodes: ['a', 'z']});
-  return {extract, compare, name: 'ISBN'};
+  const IDENTIFIER_NAME = 'ISBN';
+
+  function validatorAndNormalizer(string) {
+    const isbnParseResult = isbnParse(string);
+    debugData(`isbnParseResult: ${JSON.stringify(isbnParseResult)}`);
+    if (isbnParseResult === null) {
+      debug(`Not parseable ISBN, just removing hyphens`);
+      return {valid: false, value: string.replace(/-/ug, '')};
+    }
+    debug(`Parseable ISBN, normalizing to ISBN-13`);
+    return {valid: true, value: isbnParseResult.isbn13};
+  }
+
+  const {extract, compare} = createInterface({identifier: IDENTIFIER_NAME, pattern: /^020$/u, subfieldCodes: ['a', 'z'], validIdentifierSubfieldCodes: ['a'], invalidIdentifierSubfieldCodes: ['z'], validatorAndNormalizer});
+  return {extract, compare, name: IDENTIFIER_NAME};
 };
+
