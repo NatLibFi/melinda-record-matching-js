91961d5c25a0e463651960c1e4e2802705336d26
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-08-22 17:15:30 +0300

    Add development comment

--- src/match-detection/features/bib/other-standard-identifier.js
+++ src/match-detection/features/bib/other-standard-identifier.js
@@ -28,6 +28,8 @@
 
 import createInterface from './standard-identifier-factory';
 
+// DEVELOP: we should compare indicators and sf2 for f024
+
 export default () => {
   const {extract, compare} = createInterface({pattern: /^024$/u, subfieldCodes: ['a', 'z']});
   return {extract, compare, name: 'Other standard identifier'};
