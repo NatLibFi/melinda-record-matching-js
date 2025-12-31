c0d046a4b69d19878af21ec4236d0d1e054ed44f
nvolk <nicholas.volk@helsinki.fi>
2025-12-31 11:31:59 +0200

    773 host/comp check takes also 973 into account

--- src/match-detection/features/bib/host-component.js
+++ src/match-detection/features/bib/host-component.js
@@ -1,6 +1,6 @@
 
 export default () => ({
   name: 'Host/Component record',
-  extract: ({record}) => record.get(/^773$/u).length > 0 ? ['component'] : ['host'],
+  extract: ({record}) => record.get(/^[79]73$/u).length > 0 ? ['component'] : ['host'],
   compare: (a, b) => a[0] === b[0] ? 0.0 : -1.0
 });
