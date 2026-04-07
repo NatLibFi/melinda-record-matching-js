2f91ce58d1225d2dbb2b5202b1f7178b3faaae69
nvolk <nicholas.volk@helsinki.fi>
2026-04-07 14:13:59 +0300

    Tune

--- src/match-detection/features/bib/title.js
+++ src/match-detection/features/bib/title.js
@@ -3,9 +3,9 @@ import naturalPkg from 'natural';
 const {LevenshteinDistance: leven} = naturalPkg;
 import {testStringOrNumber} from '../../../matching-utils.js';
 
-const debug = createDebugLogger('@natlibfi/melinda-record-matching:match-detection:features:title');
-const debugData = debug.extend('data');
 
+const debug = createDebugLogger('@natlibfi/melinda-record-matching:match-detection:features/bib/title');
+const debugData = debug.extend('data');
 
 export default ({threshold = 0.9} = {}) => ({
   name: 'Title',
@@ -15,13 +15,13 @@ export default ({threshold = 0.9} = {}) => ({
     debug(`${label}: title: ${a}`);
 
     if (a) {
-      const b = normalizeTitle(getTitle(record, ['b']));
-      const n = normalizeTitle(getTitle(record, ['n']));
-      const p = normalizeTitle(getTitle(record, ['p']));
+      const b = normalizeTitle(getTitle(record, ['b'])) || '';
+      const n = normalizeTitle(getTitle(record, ['n'])) || '';
+      const p = normalizeTitle(getTitle(record, ['p'])) || '';
       return [normalizeTitle(a), b, n, p];
     }
 
-    return [];
+    return ['', '', '', ''];
 
     function normalizeTitle(title) {
       return title
@@ -37,40 +37,71 @@ export default ({threshold = 0.9} = {}) => ({
 
   },
   compare: (a, b) => {
-    const debug = createDebugLogger('@natlibfi/melinda-record-matching:match-detection:features/bib/title');
+
     const [aa, ab, an, ap] = a;
     const [ba, bb, bn, bp] = b;
-    const aFull = toFullTitle(a);
-    const bFull = toFullTitle(b);
-
-    if (an && bn && an !== bn) { // If these exists, they must be the same (we might convert Roman numbers to Arabic numbers though)
+    if (isEmpty(aa) || isEmpty(ba)) {
+      return -1.0;
+    }
+    // F245$n information can not mismatch:
+    if (an.length && bn.length && an !== bn) { // If these exists, they must be the same (we might convert Roman numbers to Arabic numbers though)
       return -1.0;
     }
 
-    const distance = leven(aFull, bFull);
+    const aFull = toFullTitle(a);
+    const bFull = toFullTitle(b);
+
+    const [distance, maxLength, correctness] = doLevenshtein(aFull, bFull);
+
+    debug(`'${aFull}' vs '${bFull}': Max length = ${maxLength}, distance = ${distance}, correctness = ${correctness}`);
 
     if (distance === 0) {
       return 0.5;
     }
 
-    const maxLength = getMaxLength();
-    const correctness = 1.0 - (distance / maxLength);
-
-    debug(`'${aFull}' vs '${bFull}': Max length = ${maxLength}, distance = ${distance}, correctness = ${correctness}`);
-
     if (correctness >= threshold) {
       return 0.3;
     }
 
-    return -0.5;
+    // Try the same without $p
+    if (localXor(ap, bp)) {
+      const result = compare([aa, ab, an, ''], [ba, bb, bn, '']);
+      return result > 0.0 ? result * 0.8 : result;
+    }
+    if (isEmpty(ap) && isEmpty(bp) && localXor(ab, bb)) {
+      // Try the same without $b ($p is not here)
+      const result = compare([aa, '', an, ''], [ba, '', bn, '']);
+      return result > 0.0 ? result * 0.8 : result;
+    }
+
+    return -0.5; // Not likely
+
+    function isEmpty(x) {
+      return !x || x.length === 0;
+    }
+
+    function localXor(x, y) {
+      if (isEmpty(x)) {
+        return !isEmpty(y);
+      }
+      // 'x' exists, thus 'y' can not exist:
+      return isEmpty(y);
+    }
+
+    function doLevenshtein(string1, string2) {
+      const distance = leven(string1, string2);
+      const len = getMaxLength(string1, string2);
+      const correctness = 1.0 - (distance / len);
+      return [distance, len, correctness];
+    }
 
     function toFullTitle(arr) {
       const relevant = arr.filter(val => typeof val === 'string' && val.length);
       return relevant.join(' ');
     }
 
-    function getMaxLength() {
-      return aFull.length > bFull.length ? aFull.length : bFull.length;
+    function getMaxLength(str1, str2) {
+      return str1.length > str2.length ? str1.length : str2.length;
     }
 
   }
