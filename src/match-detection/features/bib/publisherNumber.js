ac193678abb4bee97cab7de7757a4b8bebadc38e
nvolk <nicholas.volk@helsinki.fi>
2026-05-26 13:28:10 +0300

    New file

---
+++ src/match-detection/features/bib/publisherNumber.js
@@ -0,0 +1,64 @@
+import {uniqArray} from '@natlibfi/marc-record-validators-melinda/dist/utils.js';
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/melinda-record-matching:match-detection:features:publisher-number');
+const debugData = debug.extend('data');
+
+export default () => ({
+  name: 'Publisher or Distributor Number',
+  extract: ({record/*, recordExternal*/}) => {
+    //const label = recordExternal && recordExternal.label ? recordExternal.label : 'record';
+
+    return getIdentifiers();
+
+    function getIdentifiers() {
+      const values = record.get(/^028$/u).map(f => fieldToPublisherNumber(f)).filter(v => v);
+
+      debug(`\t ${values.length} potential publisher identifier fields (${fields[0].tag}): ${values.join(', ')}`);
+
+      return uniqArray(values);
+
+      function fieldToPublisherNumber(field) {
+        const a = field.subfields.find(sf => sf.code === 'a');
+        if (!a || !a.value) {
+          return undefined;
+        }
+        const b = field.subfields.find(sf => sf.code === 'b');
+        if (!b || !b.value) {
+          return normalizePublisherNumber(a.value);
+        }
+        return normalizePublisherNumber(`${b.value}{$a.value}`);
+      }
+
+      function normalizePublisherNumber(val) {
+        return val.replace(/[- .,:;]/ug, '');
+      }
+    }
+  },
+  compare: (aa, bb) => {
+    debugData(`Comparing ISSN sets ${JSON.stringify(aa)} and ${JSON.stringify(bb)}`);
+    if (aa.length === 0 || bb.length === 0) {
+      // No data for decision
+      return 0;
+    }
+    const firstSharedIssn = aa.find(val => bb.includes(val));
+    if (firstSharedIssn) {
+      debug(`\t Shared ISSN found: '${firstSharedIssn}'`);
+      // Maybe less for comps?
+      return 0.2;
+    }
+
+    const aa2 = aa.map(a => a.replace(/[^0-9]/ug, ''));
+    const bb2 = bb.map(b => b.replace(/[^0-9]/ug, ''));
+
+    // Numbers matches (aka don't penalize "Fazer F-123" vs "F-123")
+    // Note that FOOLP-123 vs FOOCD-123 will now also return 0.0. Still nothing positive is returned, so no damage done.
+    if (aa2.find(val >= val !== '' && bb.includes(val))) {
+      return 0.0;
+    }
+
+    return -0.2;
+
+  }
+});
+
